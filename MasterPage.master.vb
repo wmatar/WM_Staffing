e3da37ea9d22bc86fcada86b5b2f1243bd90e543
Waleed <wal_matar@hotmail.com>
2013-11-06 12:15:32 -0600

    added today's date to the master page

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -1,5 +1,10 @@
 ﻿
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        lbl_date.Text = "Welcome to WM Staffing! Today's date: " & Date.Today
+
+    End Sub
 End Class
 
